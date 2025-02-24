79a204ac0e92e428e3fa33553fa3d811cccc823f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-24 19:17:36 +0530

    Update

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -24,6 +25,18 @@ const authController = {
                     { username: username },
                     { email: email },
                 ]
+            })
+
+            if(checkuser){
+                return res.json({ Error: "User Already Exists"})
+            }
+
+            const hashpass = await bcrypt.hash(password, 10)
+
+            const newuser = new User({
+                username: username,
+                email: email,
+                password: hashpass
             })
         }
         catch(err){
---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'moderator', 'user'], required: true, default: "user" },  
+}, {timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
