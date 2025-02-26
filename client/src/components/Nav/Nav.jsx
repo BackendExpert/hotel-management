acb4c20f2828b851c0c701979635f4d42b55b440
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-26 08:43:42 +0530

    Update Nav.jsx

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaStar } from "react-icons/fa6";
 import DefultBtn from '../Buttons/DefultBtn';
 import { navdata } from './NavData';
+import { RiMenu4Fill, RiCloseLargeFill } from "react-icons/ri";
+
 
 const Nav = () => {
+    const [opennav, setopennav] = useState(false)
+
+    const toggleopenmenu = () => {
+        setopennav(!opennav)
+    }
   return (
-    <div className='py-6 md:px-32 px-8 w-full bg-white shadow-md'>
+    <div className='py-6 xl:px-32 px-8 w-full bg-white shadow-md'>
       <div className="flex justify-between">
         <div>
-          <h1 className="text-[#4e5c4a] uppercase tracking-[.5em] text-black text-3xl">
+          <h1 className="text-[#4e5c4a] uppercase md:tracking-[.5em] text-black text-3xl">
             MyHotels
           </h1>
           <div className="flex justify-center mt-1">
-            <div className="flex gap-4">
+            <div className="flex md:gap-4 gap-2">
               <FaStar className='h-2 w-auto fill-[#a4805a]' />
               <FaStar className='h-2 w-auto fill-[#a4805a]' />
               <FaStar className='h-2 w-auto fill-[#a4805a]' />
@@ -23,7 +30,15 @@ const Nav = () => {
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="xl:hidden block mt-1">
+            {
+                !opennav ? 
+                    <RiMenu4Fill className='h-10 w-auto fill-[#4e5c4a]' onClick={toggleopenmenu}/>
+                :
+                    <RiCloseLargeFill className='h-10 w-auto fill-[#4e5c4a]' onClick={toggleopenmenu}/>
+            }
+        </div>
+        <div className="xl:block hidden">
             <div className="flex">
                 <div className="flex mr-8">
                     {
