33850d7e2d2a825577e6504ddcedfdb5858303b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-26 08:34:18 +0530

    Update

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { FaStar } from "react-icons/fa6";
+import DefultBtn from '../Buttons/DefultBtn';
 
 const Nav = () => {
-
   return (
-    <div className='py-4 md:px-32 px-8 w-full bg-white shadow-md'>
-        <div className="flex justify-between">
-            <div className="flex ">
-                <h1 className="logo-title text-black text-3xl">GoBidly</h1>
-                <div className="pl-4 text-black">
-                    <h1 className="font-semibold">Auction</h1>
-                    <p className="">Platform</p>
+    <div className='py-6 md:px-32 px-8 w-full bg-white shadow-md'>
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-[#4e5c4a] uppercase tracking-[.5em] text-black text-3xl">
+            MyHotels
+          </h1>
+          <div className="flex justify-center mt-1">
+            <div className="flex gap-4">
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+              <FaStar className='h-2 w-auto fill-[#a4805a]' />
+            </div>
+          </div>
+        </div>
+        <div className="">
+            <div className="">
+                <div className=""></div>
+                <div className="">
+                    <DefultBtn 
+                        type={'button'}
+                        btnvlaue={'Book Now'}
+                    />
                 </div>
             </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
---
+++ client/src/components/Nav/NavData.jsx
@@ -0,0 +1,71 @@
+const navdata = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/",
+        submenu: []
+    },
+    {
+        id: 2,
+        name: "Rooms",
+        link: "/",
+        submenu: [
+            {
+                id: 1,
+                name: "Single Room",
+                link: "#"
+            },
+            {
+                id: 2,
+                name: "Double Room",
+                link: "#"
+            },
+            {
+                id: 3,
+                name: "Family Room",
+                link: "#"
+            },
+            {
+                id: 4,
+                name: "Deluxe Double Room",
+                link: "#"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Pages",
+        link: "/",
+        submenu: [
+            {
+                id: 1,
+                name: "About Hotem",
+                link: "#"
+            },
+            {
+                id: 1,
+                name: "Restaurant & Bars",
+                link: "#"
+            },
+            {
+                id: 1,
+                name: "Spa",
+                link: "#"
+            },
+            {
+                id: 1,
+                name: "Events",
+                link: "#"
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "news",
+        link: "/",
+        submenu: []
+    },
+
+]
+
+export {navdata}
