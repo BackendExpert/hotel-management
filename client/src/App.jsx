f0cf87fa4b9d22bc983b75eec3fcdc4abde5e19e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-26 08:12:22 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,60 @@
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Home from "./pages/HomePage/Home";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+
+
 
 export default function App() {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 50) {
+
+      setShowNavBar(false);
+    } else {
+
+      setShowNavBar(true);
+    }
+    setIsTopOfPage(currentScrollY === 0);
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const shouldShowNavBar = !location.pathname.startsWith("/Dashboard");
+  const shouldShowFooter = !location.pathname.startsWith("/Dashboard");
   return (
     <BrowserRouter>
+        {shouldShowNavBar && (
+          <div
+          className={`fixed top-0 w-full z-50 transition-transform duration-300 ${
+            showNavBar ? "translate-y-0" : "-translate-y-full"
+          } ${
+            isTopOfPage
+              ? "bg-transparent transition-colors duration-500"
+              : "bg-[#7B3F00] shadow-md transition-colors duration-500"
+          }`}
+          >
+            <Nav />
+          </div>
+        )}
       <Routes>
         <Route path="/" element={<Home /> } />
       </Routes>
+      {shouldShowFooter && <Footer />}
     </BrowserRouter>
   )
 }
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Nav = () => {
+
   return (
-    <div>Nav</div>
+    <div className='py-4 md:px-32 px-8 w-full bg-white shadow-md'>
+        <div className="flex justify-between">
+            <div className="flex ">
+                <h1 className="logo-title text-black text-3xl">GoBidly</h1>
+                <div className="pl-4 text-black">
+                    <h1 className="font-semibold">Auction</h1>
+                    <p className="">Platform</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
