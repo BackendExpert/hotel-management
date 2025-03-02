df1f685c6c4281d041714b0611ebce4e82c096a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-02 10:52:56 +0530

    Update

---
+++ client/src/pages/Dashboard/AdminDashbaord.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDashbaord = () => {
+  return (
+    <div>AdminDashbaord</div>
+  )
+}
+
+export default AdminDashbaord
--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import secureLocalStorage from "react-secure-storage";
 
 const DashHome = () => {
+    const RoleUser = secureLocalStorage.getItem('loginR')
+    const EmailUser = secureLocalStorage.getItem('loginE')
+    const Username = secureLocalStorage.getItem('loginU')
   return (
     <div>DashHome</div>
   )
