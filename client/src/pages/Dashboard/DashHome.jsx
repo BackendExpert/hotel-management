d677f47bb25e08d05168eb815189a0e70083770f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-02 10:54:25 +0530

    Update DashHome.jsx

--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import secureLocalStorage from "react-secure-storage";
+import AdminDashbaord from './AdminDashbaord';
+import StaffDashboard from './StaffDashboard';
 
 const DashHome = () => {
     const RoleUser = secureLocalStorage.getItem('loginR')
     const EmailUser = secureLocalStorage.getItem('loginE')
     const Username = secureLocalStorage.getItem('loginU')
   return (
-    <div>DashHome</div>
+    <div>
+        {
+            (() => {
+                if(RoleUser === "admin"){
+                    return ( <AdminDashbaord /> )
+                }
+                else if(RoleUser === "staff"){
+                    return ( <StaffDashboard /> )
+                }
+            })()
+        }
+    </div>
   )
 }
 
